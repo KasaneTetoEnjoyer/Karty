adc9926fa7e9cb80c449dd000e7f9c2a3115ea8a
KasaneTetoEnjoyer <2002stas@gmail.com>
2025-05-04 17:37:11 +0200

    Add project files.

---
+++ Karty/MainWindow.axaml.cs
@@ -0,0 +1,29 @@
+using Avalonia.Controls;
+using Avalonia.Interactivity;
+
+namespace Karty;
+
+
+public partial class MainWindow : Window
+{
+    public MainWindow()
+    {
+        InitializeComponent();
+    }
+
+    private void Pasjans_Click(object? sender, RoutedEventArgs e)
+    {
+
+        //new PasjansWindow().Show();
+    }
+
+    private void Wojna_Click(object? sender, RoutedEventArgs e)
+    {
+        //new WojnaWindow().Show();
+    }
+
+    private void Makao_Click(object? sender, RoutedEventArgs e)
+    {
+        //new MakaoWindow().Show();
+    }
+}
